4ec89e46e5194c942e601566f5f721abda0b27f6
Gayathri.M <gayathrimurugavel20@gmail.com>
2026-06-30 18:27:30 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="**************",
+    password="YOUR_PASSWORD",
     database="road_damage_db"
 )
 
